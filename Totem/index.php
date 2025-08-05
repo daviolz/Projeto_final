91186c21cc566cc7fe9ee06613649702184eae27
Pedro Luiz Alves <pedrolsa2006@gmail.com>
2025-08-04 23:26:23 -0300

    Responsividade para celular parcialmente pronta

--- Totem/index.php
+++ Totem/index.php
@@ -3,138 +3,110 @@
 
 <head>
     <link rel="icon" type="image/png" href="../img/Comes-_1_.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link href="https://fonts.googleapis.com/css2?family=Huninn&display=swap" rel="stylesheet">
-    <style>
-        body {
-            background-color: #AB744B;
-        }
-
-        #img1 {
-            position: fixed;
-            top: -1%;
-            left: -1%;
-            width: 120%;
-            height: 120%;
-
-        }
-
-        @keyframes opacidade {
-            0% {
-                opacity: 1;
+    <link rel="stylesheet" href="css/index.css">
+    <!-- <style>
+        @media screen and (max-width: 450px) {
+            body {
+                margin: 0;
+                padding: 0;
+                background-color: #ab744b;
+                overflow: hidden;
             }
 
-            50% {
-                opacity: 0;
+            .imagem {
+                position: fixed;
+                top: -1%;
+                left: -1%;
+                width: 101%;
+                height: 101%;
+                object-fit: cover;
             }
 
-            100% {
-                opacity: 1;
+            #img1 {
+                position: fixed;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                height: 100%;
+            }
+
+            #img5 {
+                top: 60%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                height: 70%;
+                width: auto;
+            }
+
+            #img6,
+            #img7,
+            #img8 {
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                height: 70%;
+                width: auto;
+            }
+
+            #img6 {
+                z-index: 1000;
+                animation: opacidade 5s linear infinite;
+            }
+
+            #img7,
+            #img8 {
+                z-index: 1000;
+            }
+
+            @keyframes opacidade {
+                0% {
+                    opacity: 1;
+                }
+
+                50% {
+                    opacity: 0;
+                }
+
+                100% {
+                    opacity: 1;
+                }
+            }
+
+            #img9 {
+                z-index: 1;
+            }
+
+            button {
+                color: rgb(255, 255, 255);
+                position: fixed;
+                top: -10%;
+                left: -35%;
+                right: 0%;
+                z-index: 9999;
+                pointer-events: auto;
+                width: 200%;
+                height: 200%;
+                background-color: transparent;
+            }
+
+            h2 {
+                text-align: center;
+                font-size: 17px;
+                font-family: "Huninn", sans-serif;
+                font-weight: 500;
+                font-style: normal;
+                text-transform: uppercase;
+                left: -10%;
+            }
+
+            button:active {
+                background-color: white;
             }
         }
-
-        #img2 {
-            position: fixed;
-            top: -1%;
-            left: -1%;
-            width: 101%;
-            height: 101%;
-
-        }
-
-        #img3 {
-            position: fixed;
-            top: -1%;
-            left: -1%;
-            width: 101%;
-            height: 101%;
-
-        }
-
-        #img4 {
-            position: fixed;
-            top: -1%;
-            left: -1%;
-            width: 101%;
-            height: 101%;
-
-
-        }
-
-        #img5 {
-            position: fixed;
-            height: 100%;
-
-        }
-
-        #img6 {
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            z-index: 1000;
-            height: 100%;
-            animation: opacidade 5s linear infinite;
-
-        }
-
-        #img7 {
-            position: fixed;
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            z-index: 1000;
-            height: 100%;
-
-        }
-
-        #img8 {
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            z-index: 1000;
-            height: 100%;
-
-        }
-
-        #img9 {
-            position: fixed;
-            top: -1%;
-            left: -1%;
-            width: 101%;
-            height: 101%;
-        }
-
-        button {
-            color: rgb(255, 255, 255);
-            position: fixed;
-            top: -10%;
-            left: -40%;
-            right: 0%;
-            z-index: 9999;
-            pointer-events: auto;
-            width: 200%;
-            height: 200%;
-            background-color: transparent;
-            cursor: pointer
-        }
-
-        h2 {
-            text-align: center;
-            font-size: 40px;
-            font-family: "Huninn", sans-serif;
-            font-weight: 500;
-            font-style: normal;
-            text-transform: uppercase;
-            left: -10%;
-
-        }
-
-        button:active {
-            background-color: white;
-        }
-    </style>
+    </style> -->
     <title>Tela inicial</title>
 </head>
 
